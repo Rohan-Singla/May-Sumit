a3ab89f0e79322b860a7771c193faf60c56a15c3
Rohan Singla <166916371+Rohan-Singla@users.noreply.github.com>
2024-05-24 00:07:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timeline.from(".mybar", {
         right: -100 + "%",
-        stagger: 0.2,
-        duration: 0.4,
+        stagger: 0.1,
+        duration: 0.3,
     })
     timeline.from(".loader2", {
         right: -100 + "%",
-        duration: 0.3,
+        duration: 0.5,
+        delay: 0.1
     })
     timeline.from(".bgimg", {
         scale: 0.5,
@@ -32,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var openmenu = document.getElementById("openmenu");
 var closemenu = document.getElementById("closemenu");
-var menutimeline = gsap.timeline({paused:true});
+var menutimeline = gsap.timeline({paused:true}); 
 menutimeline.to(".menubarbg", {
     left: 0,
     duration: .5,
@@ -48,3 +49,23 @@ function openmenu2() {
 function myfnc() {
     menutimeline.reverse();
 }
+
+var body = document.querySelector("body");
+var cursor = document.querySelector(".cursor");
+var blackcursor = document.querySelectorAll(".cursor-black")
+body.addEventListener("mousemove",function(e){
+    gsap.to(cursor,{
+        x:e.x,
+        y:e.y,
+        duration:.5,
+    })
+})
+
+blackcursor.forEach(link=>{
+    link.addEventListener('mouseleave',()=>{
+        cursor.classList.remove("black-cursor")
+    })
+    link.addEventListener('mousemove',()=>{
+        cursor.classList.add("black-cursor")
+    })
+})
